394f8ebc2727ab55ae2a04ebe7693ddc867e5a7f
Bekk Blando <bekkblando@gmail.com>
2015-06-20 19:02:15 -0400

    Hooked Up
    Take a look at hypothesis

--- BekksRoom/messy.py
+++ BekksRoom/messy.py
@@ -1,15 +1,26 @@
 from BekksRoom.stats_functions import average_minutes, stdev_minutes
 from BekksRoom.stats_functions import hypothesis_test_plot
+import pandas as pd
 
 
 def get_input(dataset):
     done = False
-    avg = input("want the average? y/n")
-    clean = input("Want your data cleaned?  We drop zeros and nan. y/n")
+    avg = input("want the average? y/n ")
+    clean = input("Want your data cleaned?  We drop zeros and nan. y/n ")
     columns = []
     if avg == "y":
-        print("Automatically including weight and household because you want the average")
+        print("Automatically including weight because you want the average")
         columns.append('weight')
+    else:
+        std = input("Want standard Deviation? y/n ")
+        if std == 'y':
+            print("Automatically including weight because you want the standard dev")
+            columns.append('weight')
+    if avg != 'y' or std != 'y':
+        hypothesis = input("Want hypothesis testing? y/n ")
+        if hypothesis == 'y':
+            print("Automatically including weight because you want hypothesis test")
+            columns.append('weight')
     for item in dataset.columns.tolist():
         print(item)
     while not done:
@@ -20,9 +31,12 @@ def get_input(dataset):
             break
         if column == 'CLEAR':
             columns = []
-        columns.append(column)
+        if column in dataset.columns.tolist():
+            columns.append(column)
+        else:
+            print("Not in dataset")
         print(columns)
-    return(avg, clean, columns)
+    return(avg, clean, columns, std, hypothesis)
 
 
 def querys(datawork, query):
@@ -30,19 +44,26 @@ def querys(datawork, query):
 
 
 def datasets(dataset, query=None):
-    avg, clean, columns = get_input(dataset)
+    avg, clean, columns, std, hypothesis = get_input(dataset)
     # print(avg, clean, columns)
     data = dataset[columns]
     if query is not None:
         data = querys(data, query)
     data.describe()
+    if std == 'y':
+        newdataframe = {}
+        for item in data.columns.tolist():
+            if item == "weight":
+                pass
+            else:
+                newdataframe[item] = [stdev_minutes(data, item)]
+        data = pd.DataFrame.from_dict(newdataframe, orient='columns', dtype=None)
     if avg == 'y':
         newdataframe = {}
         for item in data.columns.tolist():
             if item == "weight":
                 pass
             else:
-                # print(data.columns.tolist())
                 newdataframe[item] = [average_minutes(data, item)]
         data = pd.DataFrame.from_dict(newdataframe, orient='columns', dtype=None)
     if clean == 'y':
@@ -51,6 +72,10 @@ def datasets(dataset, query=None):
                 pass
             else:
                 data = data[data[item] > 0]
+                data = data.dropna()
                 # print(data)
-        return data
+    if hypothesis == 'y':
+        group_var = input("What is your group variable?")
+        test_var = input("What is your test variable?")
+        data = hypothesis_test_plot(data, group_var, test_var)
     return data
